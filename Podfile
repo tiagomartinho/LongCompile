3b9e874e6b3ba6fc218d6495f24f0b3e848aa1b5
Tiago Martinho <t.martinho@icloud.com>
2018-09-21 19:10:58 +0200

    Add Dependency

--- Podfile
+++ Podfile
@@ -1,10 +1,7 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+inhibit_all_warnings!
+use_frameworks!
 
 target 'LongCompile' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
-  use_frameworks!
-
+  platform :ios, '9.3'
   pod 'MobileVLCKit', '~> 3.1.4'
-
 end
---
+++ framework/framework.h
@@ -0,0 +1,19 @@
+//
+//  framework.h
+//  framework
+//
+//  Created by Tiago Martinho on 21/9/18.
+//  Copyright © 2018 tm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for framework.
+FOUNDATION_EXPORT double frameworkVersionNumber;
+
+//! Project version string for framework.
+FOUNDATION_EXPORT const unsigned char frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework/PublicHeader.h>
+
+
